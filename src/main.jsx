67d0dc3a52f7f0cc5236a4da8cfeb868511b873d
Gary HL <garyhl321@gmail.com>
2023-06-24 01:50:34 +0200

    config i18next

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,35 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { AuthProvider } from './context/AuthContext.jsx'
+import i18next from 'i18next'
+import { I18nextProvider } from 'react-i18next'
+
+import global_es from './translations/es/global.json'
+import global_en from './translations/en/global.json'
+import global_fr from './translations/fr/global.json'
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: "es",
+  resources: {
+    es: {
+      global: global_es
+    },
+    en: {
+      global: global_en
+    },
+    fr: {
+      global: global_fr
+    }
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <I18nextProvider i18n={i18next}>
+        <App />
+      </I18nextProvider>
     </AuthProvider>
   </React.StrictMode>,
 )
