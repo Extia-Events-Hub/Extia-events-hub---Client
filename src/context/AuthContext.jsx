28d292c60b8dc8125a0f8d4dce0762d54fb93ecb
Gary HL <garyhl321@gmail.com>
2023-06-25 13:28:12 +0200

    formated authContext

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,27 +1,25 @@
-import { createContext, useState } from "react";
+import React, { createContext } from "react";
 import { useLocalStorage } from "../hooks/useLocalStorage";
-
 
 const AuthContext = createContext(undefined);
 
-
 export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useLocalStorage("user", "");
-    const [token, setToken] = useLocalStorage("token", "");
-    const [lenguage, setLenguage] = useLocalStorage("lenguage","ES")
+  const [user, setUser] = useLocalStorage("user", "");
+  const [token, setToken] = useLocalStorage("token", "");
+  const [lenguage, setLenguage] = useLocalStorage("lenguage", "ES");
 
-    const contextData = {
-        user,
-        setUser,
-        token,
-        setToken,
-        lenguage,
-        setLenguage,
-    };
+  const contextData = {
+    user,
+    setUser,
+    token,
+    setToken,
+    lenguage,
+    setLenguage,
+  };
 
-    return (
-        <AuthContext.Provider value={contextData}>{children}</AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider value={contextData}>{children}</AuthContext.Provider>
+  );
 };
 
-export default AuthContext
+export default AuthContext;
