3c7f3aed4b212ccf9f00d5d7e4c6ea0b2765c38c
Gary HL <garyhl321@gmail.com>
2023-06-25 13:27:12 +0200

    formated Button

--- src/components/ui/commons/Button.jsx
+++ src/components/ui/commons/Button.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React from "react";
 
 function Button({ children, type, className, onClick, disabled }) {
-    return (
-        <button
-            type={type}
-            className={'btn bg-primary text-white font-bold hover:border-primary font-mulish  hover:bg-transparent hover:text-primary shadow-lg ' + ' ' + className}
-            onClick={onClick}
-            disabled={disabled}
-        >
-            {children}
-        </button>
-    );
+  return (
+    <button
+      type={type}
+      className={
+        "btn bg-primary text-white font-bold hover:border-primary font-mulish  hover:bg-transparent hover:text-primary shadow-lg " +
+        " " +
+        className
+      }
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
 }
 
 export default Button;
