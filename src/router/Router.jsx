f165a4b030b1d02d05cdbbc48a19a0754a24b79d
Gary HL <garyhl321@gmail.com>
2023-06-25 13:29:04 +0200

    formated Router

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,47 +1,45 @@
-import React, { useContext } from 'react'
-import { HashRouter, Route, Routes, Navigate } from 'react-router-dom'
-import Home from '../views/home/Home'
-import Login from '../views/auth/Login'
-import Register from '../views/auth/Register'
-import NotFound from '../components/ui/commons/NotFound'
-import AuthContext from '../context/AuthContext'
-import Events from '../views/events/Events'
-import SingleEvent from '../views/singleEvent/SingleEvent'
-import AdminView from '../views/admin/AdminView'
-import CreateEvent from '../views/admin/CreateEvent'
+import React, { useContext } from "react";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
+import Home from "../views/home/Home";
+import Login from "../views/auth/Login";
+import Register from "../views/auth/Register";
+import NotFound from "../components/ui/commons/NotFound";
+import AuthContext from "../context/AuthContext";
+import Events from "../views/events/Events";
+import SingleEvent from "../views/singleEvent/SingleEvent";
+import AdminView from "../views/admin/AdminView";
+import CreateEvent from "../views/admin/CreateEvent";
 
 export function Router() {
+  const { token } = useContext(AuthContext);
 
-    const { token } = useContext(AuthContext);
+  const isAuthenticated = token !== null && token !== undefined && token !== "";
 
-    const isAuthenticated = token !== null && token !== undefined && token !== '';
+  const ProtectedRoute = ({ element: Element, ...props }) => {
+    return isAuthenticated ? (
+      <Element {...props} />
+    ) : (
+      <Navigate to="/login" replace state={{ from: props.location }} />
+    );
+  };
+  return (
+    <HashRouter>
+      <div className="text-text-color bg-base">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
 
-    const ProtectedRoute = ({ element: Element, ...props }) => {
-        return isAuthenticated ? (
-            <Element {...props} />
-        ) : (
-            <Navigate to="/login" replace state={{ from: props.location }} />
-        )
-    }
-    return (
-        <HashRouter>
-            <div className="text-text-color bg-base">
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/events/:id" element={<SingleEvent />} />
 
-                <Routes>
-                    <Route path='/login' element={<Login />} />
-                    <Route path='/register' element={<Register />} />
+          <Route path="/admin" element={<AdminView />} />
+          <Route path="/create-event" element={<CreateEvent />} />
 
-                    <Route path='/' element={<Home />} />
-                    <Route path='/home' element={<Home />} />
-                    <Route path='/events' element={<Events />} />
-                    <Route path='/events/:id' element={<SingleEvent />} />
-
-                    <Route path='/admin' element={<AdminView/>}/>
-                    <Route path='/create-event' element={<CreateEvent/>}/>
-
-                    <Route path='*' element={<NotFound />} />
-                </Routes>
-            </div>
-        </HashRouter>
-    )
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </HashRouter>
+  );
 }
