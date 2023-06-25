c01b1ffc0794d2d5285e53ae92757720239dac50
Gary HL <garyhl321@gmail.com>
2023-06-25 13:07:26 +0200

    cleaning code with eslint standars

--- src/main.jsx
+++ src/main.jsx
@@ -1,37 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { AuthProvider } from './context/AuthContext.jsx'
-import i18next from 'i18next'
-import { I18nextProvider } from 'react-i18next'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import i18next from "i18next";
+import { I18nextProvider } from "react-i18next";
 
-import global_es from './translations/es/global.json'
-import global_en from './translations/en/global.json'
-import global_fr from './translations/fr/global.json'
+import globalEs from "./translations/es/global.json";
+import globalEn from "./translations/en/global.json";
+import globalFr from "./translations/fr/global.json";
 
 i18next.init({
   interpolation: { escapeValue: false },
   lng: "es",
   resources: {
     es: {
-      global: global_es
+      global: globalEs,
     },
     en: {
-      global: global_en
+      global: globalEn,
     },
     fr: {
-      global: global_fr
-    }
-  }
-})
+      global: globalFr,
+    },
+  },
+});
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
       <I18nextProvider i18n={i18next}>
         <App />
       </I18nextProvider>
     </AuthProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
