2910889f95555c76926227bb4ce50db20a7611d0
Gary HL <garyhl321@gmail.com>
2023-06-25 13:29:59 +0200

    formated register page

--- src/views/auth/Register.jsx
+++ src/views/auth/Register.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import FormRegister from '../../components/forms/auth/FormRegister'
-import ToggleDarkMode from '../../components/toggleDarkMode/ToggleDarkMode'
+import React from "react";
+import FormRegister from "../../components/forms/auth/FormRegister";
+import ToggleDarkMode from "../../components/toggleDarkMode/ToggleDarkMode";
 
 function Register() {
-    return (
-        <div className='w-full px-[5%] md:px-[10%] grid md:grid-cols-2 py-[10vh] min-h-screen '>
-            <FormRegister className='rounded-2xl  md:rounded-r-none justify-center lg:px-16' />
-            <div className=" bg-base-100 rounded-r-2xl shadow-custom hidden relative md:flex md:flex-col">
-                {/* Logo */}
-            </div>
-        </div>
-    )
+  return (
+    <div className="w-full px-[5%] md:px-[10%] grid md:grid-cols-2 py-[10vh] min-h-screen ">
+      <FormRegister className="rounded-2xl  md:rounded-r-none justify-center lg:px-16" />
+      <div className=" bg-base-100 rounded-r-2xl shadow-custom hidden relative md:flex md:flex-col">
+        {/* Logo */}
+      </div>
+    </div>
+  );
 }
 
-export default Register
+export default Register;
