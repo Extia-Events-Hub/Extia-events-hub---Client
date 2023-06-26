2f0d4c4817c8d6973aca4647c9957b3bf3209540
Gary HL <garyhl321@gmail.com>
2023-06-26 22:34:20 +0200

    use eventService to get Events data from api

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../../components/ui/navbar/Navbar";
 import { useTranslation } from "react-i18next";
 import { EventCard } from "../../components/ui/cards/eventCard/EventCard";
-import eventsData from "../../utilities/eventsData.json";
 import Footer from "../../components/ui/footer/Footer";
 import { Link } from "react-router-dom";
 import { HiArrowRight } from "react-icons/hi2";
 import HomeSection from "../../components/ui/section/homeSection/HomeSection";
 import HomeHeader from "../../components/ui/header/HomeHeader";
+import { eventService } from "../../services/events.service";
 
 function Home() {
   const { t } = useTranslation("global");
+  const [eventsData, setEventsData] = useState(null);
+
+  const getEvents = async () => {
+    const { data } = await eventService.index();
+    console.log(data.data);
+    setEventsData(data.data);
+  };
+
+  useEffect(() => {
+    getEvents();
+  }, []);
+
+  const brandList = ["", "", "", "", ""];
 
   return (
     <>
@@ -18,15 +31,21 @@ function Home() {
       <div className="w-full px-[5%] md:px-[10%] pt-[15vh] flex flex-col relative min-h-screen gap-8">
         {/* Header */}
         <HomeHeader />
-
+        
         <HomeSection
           className={"pt-16 md:pt-24 md:p-0 "}
           title={t("home.events")}
         >
           <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            {eventsData?.map((event, index) => {
-              return <EventCard key={index} event={event} />;
-            })}
+            {eventsData ? (
+              eventsData?.map((event, index) => {
+                return <EventCard key={index} event={event} />;
+              })
+            ) : (
+              <div className="w-full col-span-full py-16 flex justify-center items-center">
+                <span className="loading text-primary loading-dots loading-md md:loading-lg"></span>
+              </div>
+            )}
           </div>
           <span className="w-full flex justify-center pt-16">
             <Link
@@ -43,7 +62,7 @@ function Home() {
           title={t("home.collaborators")}
         >
           <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-8">
-            {eventsData?.map((event, index) => {
+            {brandList?.map((event, index) => {
               return (
                 <div
                   key={index}
