ecf692754ecdaec08470ba333f7fc2e50ffe1abc
Gary HL <garyhl321@gmail.com>
2023-06-28 10:38:28 +0200

    use eventService in admin view

--- src/views/admin/AdminView.jsx
+++ src/views/admin/AdminView.jsx
@@ -1,34 +1,44 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Navbar from "../../components/ui/navbar/Navbar";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import EventsList from "../../components/templates/eventsList/EventsList";
-import eventsJson from "../../utilities/eventsData.json";
-import EventPreview from "../../components/templates/eventPreview/EventPreview";
+import { eventService } from "../../services/events.service";
+import { createEventAdapter } from "../../adapters/event.adapter";
+import AuthContext from "../../context/AuthContext";
 
 function AdminView() {
   const { t } = useTranslation("global");
-  const [eventSelected, setEventSelected] = useState(null);
+  const { language } = useContext(AuthContext);
 
+  const [eventsData, setEventsData] = useState([]);
+
+  const getEvents = async () => {
+    const { data } = await eventService.index();
+    const eventsAdapted = await data?.data?.map((event) => {
+      return createEventAdapter(event, language);
+    });
+    setEventsData(eventsAdapted);
+  };
+
+  useEffect(() => {
+    getEvents();
+  }, []);
   return (
     <>
       <Navbar className="fixed w-full  md:px-[10%] z-30" />
-      <div className="w-full px-[5%] md:px-[10%] pt-[15vh] flex flex-col relative min-h-screen gap-16">
-        <div className="flex flex-col md:flex-row title2 gap-4">
+      <div className="w-full px-[5%] md:px-[10%] pt-[15vh] flex flex-col relative min-h-screen gap-8">
+        <div className="flex flex-col md:flex-row title3 gap-4">
           <h1>{t("admin.viewEstatistics")}</h1>
-          <Link className="underline text-primary">
+          <Link to={"/create-event"} className="underline text-primary">
             {t("admin.createNewEvent")}
           </Link>
         </div>
         <div className="w-full flex flex-col md:flex-row gap-8 min-h-[60vh] ">
           <EventsList
-            eventsList={eventsJson}
-            setEventSelected={setEventSelected}
-            className={"md:h-[65vh]  md:w-1/2"}
-          />
-          <EventPreview
-            className={"md:h-[65vh]  md:w-1/2"}
-            eventSelected={eventSelected}
+            getEvents={getEvents}
+            eventsList={eventsData || []}
+            className={"md:h-[50vh]  md:w-full"}
           />
         </div>
       </div>
