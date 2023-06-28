5afa36f8a46f5e9a0d9e3842632f3bdc39ba72a9
Gary HL <garyhl321@gmail.com>
2023-06-28 10:18:35 +0200

    created eventJson base

---
+++ src/utilities/eventBaseJson.jsx
@@ -0,0 +1,31 @@
+export const eventBase = {
+  id: 0,
+  title: {
+    es: "---",
+    en: "---",
+    fr: "---",
+  },
+  shortDescription: {
+    es: "---",
+    en: "---",
+    fr: "---",
+  },
+  longDescription: {
+    es: "---",
+    en: "---",
+    fr: "---",
+  },
+  mode: {
+    es: { isPresential: true, location: "---" },
+    en: { isPresential: true, location: "---" },
+    fr: { isPresential: true, location: "---" },
+  },
+  startDate: "---",
+  startTime: "---",
+  endDate: "---",
+  endTime: "---",
+  user_id: 1,
+  max_participants:999,
+  image:
+    "---",
+};
