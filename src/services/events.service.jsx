6542fea12ef1881f59556b6b033c865bff48fe23
Gary HL <garyhl321@gmail.com>
2023-06-28 10:37:06 +0200

    add new route in eventService create

--- src/services/events.service.jsx
+++ src/services/events.service.jsx
@@ -6,13 +6,16 @@ export const eventService = {
   index: () => {
     return axios.get(baseUrl + "/events");
   },
+  create:(data) => {
+    return axios.post(baseUrl + "/events",data)
+  },
   show: (eventId) => {
     return axios.get(baseUrl + "/events/" + eventId);
   },
   update: (eventId, data) => {
-    return axios.put(baseUrl + "/users/" + eventId, data);
+    return axios.put(baseUrl + "/events/" + eventId, data);
   },
   delete: (eventId) => {
-    return axios.delete(baseUrl + "/users/" + eventId);
+    return axios.delete(baseUrl + "/events/" + eventId);
   },
 };
