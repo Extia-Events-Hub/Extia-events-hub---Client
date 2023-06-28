c3ccae4f97fe91d06e85d49e54922b707ba69641
Gary HL <garyhl321@gmail.com>
2023-06-28 10:26:40 +0200

    add ilustrations

--- src/views/auth/Login.jsx
+++ src/views/auth/Login.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import FormLogin from "../../components/forms/auth/FormLogin";
+import penImage from "../../assets/images/3dpen.png";
 
 function Login() {
   return (
     <div className="w-full px-[5%] md:px-[10%] grid md:grid-cols-2 py-[10vh] min-h-screen ">
       <FormLogin className="rounded-2xl md:rounded-r-none justify-center lg:px-16" />
-      <div className=" bg-base-100 rounded-r-2xl shadow-custom hidden relative md:flex md:flex-col">
-        {/* Logo */}
+      <div className=" bg-white rounded-r-2xl shadow-custom hidden relative md:flex justify-center items-center">
+        <img
+          className="w-full shadow-inner max-w-md drop-shadow-2xl "
+          src={penImage}
+          alt="pen image"
+        />
       </div>
     </div>
   );
