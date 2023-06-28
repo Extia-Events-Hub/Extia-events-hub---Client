c4060084ecb5b3c0d28db39930b2510876538bd2
Gary HL <garyhl321@gmail.com>
2023-06-28 10:35:59 +0200

    add translations in events view

--- src/views/events/Events.jsx
+++ src/views/events/Events.jsx
@@ -8,6 +8,7 @@ import FilterEvents from "../../components/filterEvents/FilterEvents";
 import { eventService } from "../../services/events.service";
 import { createEventAdapter } from "../../adapters/event.adapter";
 import AuthContext from "../../context/AuthContext";
+import ScrollToTopButton from "../../components/scrollTopButton/ScrollTopButton";
 
 function Events() {
   const { t } = useTranslation("global");
@@ -24,7 +25,7 @@ function Events() {
       return createEventAdapter(event, language);
     });
     setEventsData(eventsAdapted);
-    setFilteredEventList(eventsAdapted)
+    setFilteredEventList(eventsAdapted);
   };
 
   useEffect(() => {
@@ -36,16 +37,18 @@ function Events() {
       <Navbar className="fixed w-full  md:px-[10%] z-30" />
       <div className="w-full px-[5%] md:px-[10%]  flex flex-col relative min-h-screen gap-8">
         {/* Hero section */}
-        <div className="w-full flex flex-col justify-center items-center mt-[15vh] min-h-[40vh] md:min-h-[50vh] gap-4">
+        <div
+          data-aos="fade-up"
+          data-aos-duration="2000"
+          
+          className="w-full flex flex-col justify-center items-center mt-[15vh] min-h-[40vh] md:min-h-[50vh] gap-4"
+        >
           <span className=" font-mulish w-[90%] text-6xl -mt-[2rem] md:text-8xl -translate-y-20  opacity-5 absolute text-center overflow-hidden">
-            Extia Events Hub
+            {t("events.headerTitle")}
           </span>
-          <h1 className="title1">Extia Events Hub</h1>
-          <p className="font-roboto max-w-lg text-center">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ratione
-            quod laborum rem, reiciendis facilis magnam unde placeat fugit
-            aliquam pariatur veritatis adipisci iste eius soluta perspiciatis
-            sint! Accusamus, molestias?
+          <h1 className="title1">{t("events.headerTitle")}</h1>
+          <p className="font-roboto max-w-2xl text-center leading-7 text-neutral-500">
+            {t("events.headerDescription")}
           </p>
         </div>
         <div className="w-full  md:p-0  flex flex-col gap-8">
@@ -62,6 +65,7 @@ function Events() {
               })}
           </div>
         </div>
+        <ScrollToTopButton/>
       </div>
       <Footer className="mt-24" />
     </>
